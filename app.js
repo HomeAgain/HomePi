d686b9eb22bdcdbc34cc2f119dad4737eebf0b23
jguerin <julian.guerin@gmail.com>
2014-02-26 23:52:25 +0100

    Clean up and add hub data

--- app.js
+++ app.js
@@ -23,19 +23,19 @@ var MoistureSimulator = require('./simulators/moisture_simulator.js');
 
 var DEBUG = true;
 
+var hub_id = "HUB_1";
 
 
 if (DEBUG) {
 	var fakeSerialPort = require('./fake_serial/fake_serial.js');
 	// Create FakeSerialPort
-
 	fakeSerialPort.init({
 		simulators: [
 			TemperatureSimulator("TEMP_1"),
 			TemperatureSimulator("TEMP_2"),
 			TemperatureSimulator("TEMP_3"),
-			TemperatureSimulator("TEMP_4"),
-			TemperatureSimulator("TEMP_5"),
+			//TemperatureSimulator("TEMP_4"),
+			//TemperatureSimulator("TEMP_5"),
 
 			DoorSimulator("DOOR_1"),
 			DoorSimulator("DOOR_2"),
@@ -55,6 +55,7 @@ if (DEBUG) {
 		data = data.split('=');
 
 		jsonData = {
+			hub_id : hub_id,
 			sensor_id: data[0],
 			value: data[1],
 			modelName: data[2],
@@ -66,12 +67,31 @@ if (DEBUG) {
 
 	});
 } else {
-	var serialport = require('serialport');
-	serialport.list(function(err, ports) {
-		ports.forEach(function(port) {
-			console.log(port.comName);
-			console.log(port.pnpId);
-			console.log(port.manufacturer);
-		});
+	var serialport = require("serialport");
+	var SerialPort = serialport.SerialPort
+	var serialPort = new SerialPort("/dev/tty.usbmodem1411", {
+  		baudrate: 9600,
+  		parser: serialport.parsers.readline("\n")
 	});
+	
+	serialPort.on("open", function(){
+		serialPort.on("data", function(data) {
+		console.log("Received : " + data);
+
+		data = data.split('=');
+
+		jsonData = {
+			hub_id : hub_id,
+			sensor_id: data[0],
+			value: data[2],
+			model: data[1].split('.')[0],
+			unit: data[1],
+			created_on : Date.now()
+		};
+		httpClient.send(jsonData);
+
+
+	});
+	});
+	
 }
--- httpclient/httpclient.js
+++ httpclient/httpclient.js
@@ -29,11 +29,8 @@ function send (json){
     };
  
     var req = http.request(options, function(res) {
-      //console.log('\nSTATUS: ' + res.statusCode);
-      //console.log('HEADERS: ' + JSON.stringify(res.headers));
       res.setEncoding('utf8');
       res.on('data', function (chunk) {
-        //console.log('BODY: ' + chunk);
       });
     });
 
